668a947dd27087bf386c3484855e89838d738573
Andrej Svrček <nepoviem251@gmail.com>
2022-01-13 21:03:50 +0100

    Takes a year and returns a formatted string with the right century.

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,7 +6,41 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            throw new NotImplementedException();
+            int answer = (int)(year / 100) + ((year % 100 == 0) ? 0 : 1);
+
+            if (answer == 11)
+            {
+                return "11th century";
+            }
+
+            if (answer % 10 == 1)
+            {
+                return answer + "st century";
+            }
+
+
+            if (answer == 12)
+            {
+                return "12th century";
+            }
+
+            if (answer % 10 == 2)
+            {
+                return answer + "nd century";
+            }
+
+
+            if (answer == 13)
+            {
+                return "13th century";
+            }
+
+            if (answer % 10 == 3)
+            {
+                return answer + "rd century";
+            }
+
+            return answer + "th century";
         }
     }
 }
